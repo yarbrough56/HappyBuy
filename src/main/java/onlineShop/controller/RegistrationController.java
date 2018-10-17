5631df3ea5a3d1300fe36bfdd58f3ad635e8e1be
yarbrough56 <yarbrough56@ucla.edu>
2018-10-17 00:12:36 -0700

    Second commit.

---
+++ src/main/java/onlineShop/controller/RegistrationController.java
@@ -0,0 +1,46 @@
+package onlineShop.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import onlineShop.model.Customer;
+import onlineShop.service.CustomerService;
+
+@Controller
+public class RegistrationController {
+    
+	@Autowired
+	private CustomerService customerService;
+
+	@RequestMapping(value = "/customer/registration", method = RequestMethod.GET)
+	public ModelAndView getRegistrationForm() {
+    	return new ModelAndView("register", "customer", new Customer());
+    							//第一个是页面名字； modelAtrribute（在jsp页面里）--》对应着表单里 model package下面的customer的class --》 new 一个 customer class
+	}
+
+	@RequestMapping(value = "/customer/registration", method = RequestMethod.POST)
+	public ModelAndView registerCustomer(@Valid @ModelAttribute(value = "customer") Customer customer,
+													// 前端的表单属性自动对应model里class的值，把注册的信息发过去
+			
+        	BindingResult result) {
+    	ModelAndView modelAndView = new ModelAndView();
+   	 
+    	if (result.hasErrors()) {
+    			// 看表单是不是合法
+        	modelAndView.setViewName("register");
+        	return modelAndView;
+    	}
+    	customerService.addCustomer(customer);
+   	 
+    	modelAndView.addObject("registrationSuccess", "Registered Successfully. Login using username and password");
+    	modelAndView.setViewName("login");
+    	return modelAndView;
+	}
+}
